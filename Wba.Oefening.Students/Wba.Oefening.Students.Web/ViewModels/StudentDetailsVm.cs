89f2b1aba196bc2302d09d7a1b313469b6c109e0
RubenVDBr <Ruben.Van.Den.Broucke@student.howest.be>
2020-10-07 08:39:45 +0200

    add viewmodel and properties

---
+++ Wba.Oefening.Students/Wba.Oefening.Students.Web/ViewModels/StudentDetailsVm.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Wba.Oefening.Students.Domain;
+
+namespace Wba.Oefening.Students.Web.ViewModels
+{
+    public class StudentDetailsVm
+    {
+        public int StudentId { get; set; }
+        public string StudentName { get; set; }
+        public Course Course { get; set; }
+    }
+}
